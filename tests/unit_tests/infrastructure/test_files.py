701334cd09aa0e4f574196a4a37ec737c5563523
achevrier <adrien-externe.chevrier@dalkia.com>
2022-01-09 17:14:26 +0100

    Add CI workflow
    (cherry picked from commit ea3dfb3331b0d2353e926ec751e946ca075ece30)

--- tests/unit_tests/infrastructure/test_files.py
+++ tests/unit_tests/infrastructure/test_files.py
@@ -12,15 +12,15 @@ class Test_files:
     files = FileManager(DATA_DIR)
 
     def test_load(self):
-        """
+        df_expected = pd.DataFrame(
+            {
+                'sepal_length': {0: '5.1', 1: '4.9'},
+                'sepal_width': {0: '3.5', 1: '3'},
+                'petal_length': {0: '1.4', 1: '1.4'},
+                'petal_width': {0: '0.2', 1: '0.2'},
+                'species': {0: 'setosa', 1: 'setosa'},
+            }
+        )
+        df = self.files.load('raw/iris.csv', nrows=2, dtype='str')
 
-        Returns
-        -------
-
-        """
-        # Given
-
-        # When
-        df_tested = self.files.load('raw/iris.csv', nrows=2, dtype='str')
-        assert df_tested.equals(df_expected)
-        # TODO
+        assert df.equals(df_expected)
